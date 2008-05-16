419b50218e2d20ea9ccafa47f26ca15a2e84b47c
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-16 19:02:47 +0000

    Adding version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/improved-gallery/trunk@46317 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ improved-gallery.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: Improved Gallery
+Version: 1.0
+Description: Improves the built-in gallery in WP 2.5
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/download/photobucket-gallery/
+*/
+
+/*
+Copyright (C) 2008 scribu.net (scribu AT gmail DOT com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+add_filter('post_gallery', 'improved_gallery', 10, 2);
+add_action('wp_head', 'improved_gallery_stylesheet');
+
+function improved_gallery($output, $attr){
+	global $post;
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'orderby'    => 'menu_order ASC, ID ASC',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+	), $attr));
+
+	$id = intval($id);
+	$attachments = get_children("post_parent=$id&post_type=attachment&post_mime_type=image&orderby={$orderby}");
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $id => $attachment )
+			$output .= wp_get_attachment_link($id, $size, true) . "\n";
+		return $output;
+	}
+
+	$listtag = tag_escape($listtag);
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+
+	$output = "\t<!-- Begin Improved Gallery -->
+	<style type='text/css'>#post{$id} .gallery-item {width: {$itemwidth}%;}</style>
+	<div class='gallery' id='post{$id}'>\n";
+
+	foreach ( $attachments as $id => $attachment ) {
+		$link = wp_get_attachment_link($id, $size, true);
+		$output .= "
+		<{$itemtag} class='gallery-item'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='gallery-caption'>
+				{$attachment->post_excerpt}
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />';
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+	</div>
+	<!-- End Improved Gallery -->\n";
+
+	return $output;
+}
+
+function improved_gallery_stylesheet(){
+	$siteurl = get_option("siteurl");
+	$siteurl = rtrim($siteurl, '/') . '/';
+	$plugin_path = $siteurl . "wp-content/plugins/" . dirname(plugin_basename(__FILE__)) . "/";
+
+	echo '<link rel="stylesheet" type="text/css" media="screen" href="' . $plugin_path . '/gallery_style.css" />'."\n";
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Improved Gallery ===
+Contributors: scribu
+Donate link: http://scribu.net/download/
+Tags: gallery, images, optimisation
+Requires at least: 2.5
+Tested up to: 2.5.1
+Stable tag: trunk
+
+Improves the built-in gallery in WP 2.5
+
+== Description ==
+
+This plugin improves the gallery template in Wordpress 2.5, by making the following tweeks:
+
+1. Puts all the gallery css in a separte file, for easy customisation and **faster page load**.
+1. Adds an id attribute to the code, so that two or more galleries can have a *different* number of columns and still **display correctly** on the same page.
+
+== Installation ==
+
+1. Unzip "Custom Field Images" archive and put the folder into your "plugins" folder (`/wp-content/plugins/`).
+1. Go to `/wp-includes/` directory, find the file `media.php` and set its permissions to 757 (This should not be necesary in WP 2.5.2)
+1. Activate the plugin from the Plugins admin menu.
+
+= Advanced Usage =
+
+If you want to make it even easier to edit your gallery style, you can do this:
+
+1. Copy the styles from 'gallery-style.css' into the main 'style.css' file in your theme (`wp-content/themes/your-theme`).
+1. Edit `improved-gallery.php` and delete or comment out this line:
+
+`add_action('wp_head', 'improved_gallery_stylesheet');`
